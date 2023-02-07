b8fbda6a865e35c20dd170ae65eceb32dfee75ed
Eray Serter <erayserter@Erays-MacBook-Pro.local>
2023-02-07 16:59:52 +0300

    structural changes

--- baseemail/admin.py
+++ baseemail/admin.py
@@ -1,24 +1,169 @@
 from django.contrib import admin, messages
-from django.urls import path, reverse
-from django.template.response import TemplateResponse
+from django.urls import path, re_path
 from django.template.loader import render_to_string
-from django.http import HttpResponseRedirect, HttpResponse
-from django.utils.html import format_html
-from django.core.exceptions import ObjectDoesNotExist
 
-import json
-
-from baseemail.views import \
-    TestTemplateAPIView, \
-    TestTemplateWithoutEmailAPIView, \
-    TestTemplateWithEmailAPIView, \
-    TestTemplateWithDataAPIView, \
-    TestTemplateSelectedWithoutEmailAPIView, \
-    TestTemplateSelectedWithEmailAPIView, \
-    TestTemplateSelectedWithDataAPIView
+from rest_framework.views import APIView
 
 from baseemail.models import EmailTemplate, Country, ObjectVariableMap
 from baseemail.forms import CountryModelForm, EmailTemplateModelForm, ObjectVariableMapModelForm
+from baseemail.helpers import render_site_with_context, replace_variables, get_template_ovm_fields_json
+
+
+class TestTemplateAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        return render_site_with_context(request)
+
+
+class TestTemplateWithoutEmailAPIView(APIView):
+    def post(self, request, template_id):
+        try:
+            email_template = EmailTemplate.objects.get(pk=template_id)
+        except Exception as e:
+            print(f'Error: {e} Email Template ID: {template_id}')
+            return render_site_with_context(request)
+
+        template_string = render_to_string("admin/baseemail/EmailTemplate/emailtemplate.html")
+
+        ovm_fields_json = get_template_ovm_fields_json(email_template)
+
+        if ovm_fields_json is None:
+            messages.error(request, "This template does not matched with an ovm.")
+            return render_site_with_context(request)
+
+        return render_site_with_context(request, extra_context={
+            'email_template': template_string,
+            'ovm_fields_list': [ovm_fields_json],
+        })
+
+    def get(self, request):
+        return render_site_with_context(request)
+
+
+class TestTemplateWithEmailAPIView(APIView):
+    def post(self, request, template_id):
+        email = request.POST['email']
+        print(email)
+
+        try:
+            email_template = EmailTemplate.objects.get(pk=template_id)
+        except Exception as e:
+            print(f'Error: {e} Email Template ID: {template_id}')
+            return render_site_with_context(request)
+
+        template_string = render_to_string("admin/baseemail/EmailTemplate/emailtemplate.html")
+
+        ovm_fields_json = get_template_ovm_fields_json(email_template)
+
+        if ovm_fields_json is None:
+            messages.error(request, "This template does not matched with an ovm.")
+            return render_site_with_context(request)
+
+        return render_site_with_context(request, extra_context={
+            'email_template': template_string,
+            'ovm_fields_list': [ovm_fields_json],
+        })
+
+
+class TestTemplateWithDataAPIView(APIView):
+    def post(self, request, template_id):
+        try:
+            email_template = EmailTemplate.objects.get(pk=template_id)
+        except Exception as e:
+            print(f'Error: {e} Email Template ID: {template_id}')
+            return render_site_with_context(request)
+
+        template_string = render_to_string("admin/baseemail/EmailTemplate/emailtemplate.html")
+
+        ovm_fields_json = get_template_ovm_fields_json(email_template)
+
+        if ovm_fields_json is not None:
+            template_string = replace_variables(template_string, ovm_fields_json.get('fields'))
+        else:
+            messages.error(request, "This template does not matched with an ovm.")
+            return render_site_with_context(request)
+
+        return render_site_with_context(request, extra_context={
+            'email_template': template_string,
+            'ovm_fields_list': [ovm_fields_json],
+        })
+
+
+class TestTemplateSelectedWithoutEmailAPIView(APIView):
+    def post(self, request):
+        queryset = EmailTemplate.objects.filter(pk__in=request.POST.getlist('selected'))
+
+        ovm_fields_list = []
+
+        for query in queryset:
+            ovm_fields = get_template_ovm_fields_json(query)
+            if ovm_fields is not None:
+                ovm_fields_list.append(ovm_fields)
+
+        if len(queryset) <= 0:
+            messages.error(request, "You should first select templates to test them.")
+            return render_site_with_context(request)
+
+        if len(ovm_fields_list) <= 0:
+            messages.error(request, "Selected templates does not matched with an ovm.")
+            return render_site_with_context(request)
+
+        return render_site_with_context(request, extra_context={
+            'ovm_fields_list': ovm_fields_list,
+        })
+
+    def get(self, request):
+        return render_site_with_context(request)
+
+
+class TestTemplateSelectedWithEmailAPIView(APIView):
+    def post(self, request):
+        email = request.POST['email']
+        print(email)
+
+        queryset = EmailTemplate.objects.filter(pk__in=request.POST.getlist('selected'))
+
+        ovm_fields_list = []
+
+        for query in queryset:
+            ovm_fields = get_template_ovm_fields_json(query)
+            if ovm_fields is not None:
+                ovm_fields_list.append(ovm_fields)
+
+        if len(queryset) <= 0:
+            messages.error(request, "You should first select templates to test them.")
+            return render_site_with_context(request)
+
+        if len(ovm_fields_list) <= 0:
+            messages.error(request, "Selected templates does not matched with an ovm.")
+            return render_site_with_context(request)
+
+        return render_site_with_context(request, extra_context={
+            'ovm_fields_list': ovm_fields_list,
+        })
+
+
+class TestTemplateSelectedWithDataAPIView(APIView):
+    def post(self, request):
+        queryset = EmailTemplate.objects.filter(pk__in=request.POST.getlist('selected'))
+
+        ovm_fields_list = []
+
+        for query in queryset:
+            ovm_fields = get_template_ovm_fields_json(query)
+            if ovm_fields is not None:
+                ovm_fields_list.append(ovm_fields)
+
+        if len(queryset) <= 0:
+            messages.error(request, "You should first select templates to test them.")
+            return render_site_with_context(request)
+
+        if len(ovm_fields_list) <= 0:
+            messages.error(request, "Selected templates does not matched with an ovm.")
+            return render_site_with_context(request)
+
+        return render_site_with_context(request, extra_context={
+            'ovm_fields_list': ovm_fields_list,
+        })
 
 
 class EmailTemplateModelAdmin(admin.ModelAdmin):
@@ -39,12 +184,6 @@ class EmailTemplateModelAdmin(admin.ModelAdmin):
         }),
     )
 
-    # list_display = (
-    #     'id', 'name', 'template_name', 'email_class', 'subject', 'contents', 'variables',
-    #     'row_variables', 'to_email', 'to_email_function', 'from_email', 'related_model_application',
-    #     'related_model_name', 'attachment_type', 'additional_parameters',
-    # )
-
     list_per_page = 25
 
     search_fields = ('name', )
@@ -52,13 +191,41 @@ class EmailTemplateModelAdmin(admin.ModelAdmin):
     def get_urls(self):
         url = super().get_urls()
         custom_urls = [
-            path('test-template/', TestTemplateAPIView.as_view(), name="test-template"),
-            path('test-template/<int:template_id>/test/', TestTemplateWithoutEmailAPIView.as_view(), name="test-template-test"),
-            path('test-template/<int:template_id>/testemail/', TestTemplateWithEmailAPIView.as_view(), name="test-template-email-test"),
-            path('test-template/<int:template_id>/testdata/', TestTemplateWithDataAPIView.as_view(), name="test-template-data-test"),
-            path('test-template/test-selected/', TestTemplateSelectedWithoutEmailAPIView.as_view(), name="test-template-selected-test"),
-            path('test-template/test-selected-email/', TestTemplateSelectedWithEmailAPIView.as_view(), name="test-template-selected-email-test"),
-            path('test-template/test-selected-data/', TestTemplateSelectedWithDataAPIView.as_view(), name="test-template-selected-data-test"),
+            path(
+                r'test-template/',
+                TestTemplateAPIView.as_view(),
+                name="test-template"
+            ),
+            re_path(
+                r'^test-template/(?P<template_id>\d+)/test/$',
+                TestTemplateWithoutEmailAPIView.as_view(),
+                name="test-template-test"
+            ),
+            re_path(
+                r'^test-template/(?P<template_id>\d+)/testemail/$',
+                TestTemplateWithEmailAPIView.as_view(),
+                name="test-template-email-test"
+            ),
+            re_path(
+                r'^test-template/(?P<template_id>\d+)/testdata/$',
+                TestTemplateWithDataAPIView.as_view(),
+                name="test-template-data-test"
+            ),
+            path(
+                'test-template/test-selected/',
+                TestTemplateSelectedWithoutEmailAPIView.as_view(),
+                name="test-template-selected-test"
+            ),
+            path(
+                'test-template/test-selected-email/',
+                TestTemplateSelectedWithEmailAPIView.as_view(),
+                name="test-template-selected-email-test"
+            ),
+            path(
+                'test-template/test-selected-data/',
+                TestTemplateSelectedWithDataAPIView.as_view(),
+                name="test-template-selected-data-test"
+            ),
         ]
 
         return custom_urls + url
---
+++ baseemail/helpers.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render
+
+from baseemail.models import ObjectVariableMap, EmailTemplate
+
+import json
+
+
+def is_empty_or_whitespace(string):
+    return string == "" or string.isspace()
+
+
+def replace_variables(html, merge_variables):
+    for var in merge_variables:
+        if not is_empty_or_whitespace(merge_variables[var]):
+            html = html.replace(f'*|{var}|*', merge_variables[var])
+
+    return html
+
+
+def get_template_ovm_fields_json(email_template):
+    try:
+        ovm = ObjectVariableMap.objects.get(
+            app_label=email_template.related_model_application,
+            model_name=email_template.related_model_name
+        )
+    except Exception as e:
+        print(f'Error: {e} Email Template pk: {email_template.pk}')
+        return None
+
+    ovm_fields = json.loads(ovm.fields.replace("\'", "\""))
+
+    for field in ovm_fields:
+        if is_empty_or_whitespace(ovm_fields[field]):
+            ovm_fields[field] = None
+
+    return {
+        'template_name': email_template.name,
+        'fields': ovm_fields,
+    }
+
+
+def render_site_with_context(request, extra_context=None):
+    all_fields = EmailTemplate._meta.get_fields()
+    value_fields = [f.name for f in all_fields]
+    email_templates = EmailTemplate.objects.all()
+
+    if request.GET.get('search') is not None:
+        email_templates = email_templates.filter(name__icontains=request.GET['search'])
+
+    templates_field_values = email_templates.values_list(*value_fields)
+
+    context = {
+        'email_templates': email_templates,
+        'templates_field_values': templates_field_values,
+        'fields': all_fields,
+    }
+
+    if extra_context is not None:
+        context.update(extra_context)
+
+    return render(request, 'admin/baseemail/TestTemplate/test_template.html', context=context)
